b1ba8ad0a26962164958eebd1dd87cc389a47c55
saba <marghanias@gmail.com>
2024-07-04 23:56:54 +0400

    finish tablet

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,33 +1,48 @@
+import React, { useEffect, useState } from 'react';
 
-export default function Tabs({tab,setTab,colors,planet}) {
+export default function Tabs({ tab, setTab, colors, planet }) {
+  const [isTabletOrLarger, setIsTabletOrLarger] = useState(window.innerWidth >= 768);
 
-    const tabStyle = {
-        borderBottom: `3px solid ${colors[planet.name]}`,
-        cursor: "pointer"
-      };
+  useEffect(() => {
+    const handleResize = () => {
+      setIsTabletOrLarger(window.innerWidth >= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const getTabStyle = (currentTab) => ({
+    cursor: 'pointer',
+    backgroundColor: isTabletOrLarger && tab === currentTab ? colors[planet.name] : 'transparent',
+    borderBottom: !isTabletOrLarger && tab === currentTab ? `3px solid ${colors[planet.name]}` : 'none'
+  });
+
   return (
     <div className="tabs">
-    <span
-      className="tab"
-      style={tab === "overview" ? tabStyle : {}}
-      onClick={() => setTab("overview")}
-    >
-      OVERVIEW
-    </span>
-    <span
-      className="tab"
-      style={tab === "structure" ? tabStyle : {}}
-      onClick={() => setTab("structure")}
-    >
-      STRUCTURE
-    </span>
-    <span
-      className="tab"
-      style={tab === "geology" ? tabStyle : {}}
-      onClick={() => setTab("geology")}
-    >
-      SURFACE
-    </span>
-  </div>
-  )
+      <span
+        className="tab"
+        style={getTabStyle('overview')}
+        onClick={() => setTab('overview')}
+      >
+        OVERVIEW
+      </span>
+      <span
+        className="tab"
+        style={getTabStyle('structure')}
+        onClick={() => setTab('structure')}
+      >
+        STRUCTURE
+      </span>
+      <span
+        className="tab"
+        style={getTabStyle('geology')}
+        onClick={() => setTab('geology')}
+      >
+        SURFACE
+      </span>
+    </div>
+  );
 }
